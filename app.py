d56546958e356c4279bb334468d789f2623ad20f
AndreiRO <andysstef@yahoo.com>
2015-04-19 19:07:11 +0300

    Basic storage classes

--- app.py
+++ app.py
@@ -4,12 +4,25 @@ from flask.ext.sqlalchemy import SQLAlchemy
 app = flask.Flask(__name__)
 app.config.from_pyfile('settings.py')
 db = SQLAlchemy(app)
+#posts = db.relationship('Post', backref='author', lazy='dynamic')
+#user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
-class Message(db.Model):
+class Deck(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String)
+    name = db.Column(db.String)
+    cards = db.relationship('Card', backref='deck', lazy='dynamic')
+    
 
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    card_id = db.Column('card_id', db.Integer, db.ForeignKey("card.id"), nullable=False)   
+
+
+class Card(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String)
+    deck_id = db.Column(db.Integer, db.ForeignKey('deck.id'), nullable=False)  
+    answers= db.relationship('Answer', backref='card', lazy='dynamic')
 
 app   = flask.Flask(__name__)
 app.config['DEBUG'] = True
