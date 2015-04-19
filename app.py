443a87bc39c72930f495a1f9443287a02edce6fc
AndreiRO <andysstef@yahoo.com>
2015-04-19 18:32:55 +0300

    Postgresql setup done

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import flask
-from flask import jsonify
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = flask.Flask(__name__)
+app.config.from_pyfile('settings.py')
+db = SQLAlchemy(app)
+
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String)
+
 
 app   = flask.Flask(__name__)
 app.config['DEBUG'] = True
@@ -8,5 +18,6 @@ app.config['DEBUG'] = True
 def root():
     return 'Salut'
 
+db.create_all()
 if __name__ == '__main__':
     app.run(port=8080)
---
+++ settings.py
@@ -0,0 +1,1 @@
+SQLALCHEMY_DATABASE_URI = "postgresql://lieserver:hash@localhost/server"
